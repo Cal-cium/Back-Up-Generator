8e1b0389becc431a8c7f4b326fa68c41b23834e4
Cal-cium <Cal-cium@users.noreply.github.com>
2017-08-15 18:10:01 +0100

    Add project files.

---
+++ Back-Up Generator/frmBackupGenerator.Designer.vb
@@ -0,0 +1,128 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmBackupGenerator
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtMainFolder = New System.Windows.Forms.TextBox()
+        Me.txtFolderBackup = New System.Windows.Forms.TextBox()
+        Me.btnBackup = New System.Windows.Forms.Button()
+        Me.cbNoOverwrite = New System.Windows.Forms.CheckBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'txtMainFolder
+        '
+        Me.txtMainFolder.Location = New System.Drawing.Point(6, 19)
+        Me.txtMainFolder.Name = "txtMainFolder"
+        Me.txtMainFolder.Size = New System.Drawing.Size(432, 20)
+        Me.txtMainFolder.TabIndex = 0
+        '
+        'txtFolderBackup
+        '
+        Me.txtFolderBackup.Location = New System.Drawing.Point(6, 17)
+        Me.txtFolderBackup.Name = "txtFolderBackup"
+        Me.txtFolderBackup.Size = New System.Drawing.Size(432, 20)
+        Me.txtFolderBackup.TabIndex = 1
+        '
+        'btnBackup
+        '
+        Me.btnBackup.Location = New System.Drawing.Point(326, 115)
+        Me.btnBackup.Name = "btnBackup"
+        Me.btnBackup.Size = New System.Drawing.Size(130, 23)
+        Me.btnBackup.TabIndex = 2
+        Me.btnBackup.Text = "Back Up"
+        Me.btnBackup.UseVisualStyleBackColor = True
+        '
+        'cbNoOverwrite
+        '
+        Me.cbNoOverwrite.AutoSize = True
+        Me.cbNoOverwrite.Location = New System.Drawing.Point(6, 19)
+        Me.cbNoOverwrite.Name = "cbNoOverwrite"
+        Me.cbNoOverwrite.Size = New System.Drawing.Size(71, 17)
+        Me.cbNoOverwrite.TabIndex = 3
+        Me.cbNoOverwrite.Text = "Overwrite"
+        Me.cbNoOverwrite.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtMainFolder)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(444, 48)
+        Me.GroupBox1.TabIndex = 4
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Main Folder:"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.txtFolderBackup)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 66)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(444, 43)
+        Me.GroupBox2.TabIndex = 5
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Back Up To:"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.cbNoOverwrite)
+        Me.GroupBox3.Location = New System.Drawing.Point(12, 115)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(291, 56)
+        Me.GroupBox3.TabIndex = 6
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Settings:"
+        '
+        'frmBackupGenerator
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(476, 184)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.btnBackup)
+        Me.Name = "frmBackupGenerator"
+        Me.Text = "Back-Up Generator"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents txtMainFolder As TextBox
+    Friend WithEvents txtFolderBackup As TextBox
+    Friend WithEvents btnBackup As Button
+    Friend WithEvents cbNoOverwrite As CheckBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents GroupBox3 As GroupBox
+End Class
---
+++ Back-Up Generator/frmBackupGenerator.vb
@@ -0,0 +1,140 @@
+﻿Imports System.IO
+Public Class frmBackupGenerator
+    Dim FolderDirectory As New Hashtable
+    Dim MainFolder As String
+    Dim lstAllFiles As New List(Of String)
+    Dim lstDirectories As New List(Of String)
+    Dim CountMarker As Integer = 0
+
+    Private Sub btnBackup_Click(sender As Object, e As EventArgs) Handles btnBackup.Click
+
+        'Check textboxes aren't empty
+        If txtFolderBackup.Text = "" OrElse txtMainFolder.Text = "" Then
+            MessageBox.Show("Please fill in all boxes!", "User Error ")
+        Else
+            MainFolder = txtMainFolder.Text
+            Dim count As Integer = 0
+            FolderDirectory.Add(count, txtFolderBackup.Text)
+
+            lstAllFiles.AddRange(Directory.GetFiles(MainFolder))
+
+            'copy files that are in the primary folder
+            CopyFiles(count)
+
+            'Add directories
+            lstDirectories.AddRange(Directory.GetDirectories(MainFolder))
+
+            'start the loop to search through all the folders and back up them
+            'foldermarker is used to keep track of folder locations, especially useful as this uses recursion, as the foldermarker keeps track of folder layout
+            Dim FolderMarker As Integer = 0
+            For Each folder In lstDirectories
+                CopyDirectories(folder, count, FolderMarker)
+            Next
+
+            'Clear all lists and count, so program is ok to run again
+            lstAllFiles.Clear()
+            lstDirectories.Clear()
+            FolderDirectory.Clear()
+            count = 0
+            CountMarker = 0
+            txtFolderBackup.Text = ""
+            txtMainFolder.Text = ""
+            cbNoOverwrite.Checked = False
+
+            MessageBox.Show("All files are backed up!", "Completed")
+
+        End If
+
+    End Sub
+
+#Region "Methods"
+    ''' <summary>
+    ''' This is a method that copies files in the chosen directory into the backup location
+    ''' </summary>
+    ''' <param name="count">This parameter is used to pick the correct folder directory when copying files over</param>
+    ''' <remarks></remarks>
+    Sub CopyFiles(ByVal count As Integer)
+        Try
+            'For each file in folder grab information about that file.
+            For Each File In lstAllFiles
+                Dim FileInfo As New FileInfo(File)
+                Dim Filename As String = FileInfo.Name
+                Dim FileLocation As String = FileInfo.FullName
+                Dim FileDate As Date = FileInfo.LastWriteTime
+
+                'If the backup folder contains the file already, compare dates on file and if newer overwrite
+                If System.IO.File.Exists(Path.Combine(CStr(FolderDirectory(count)), Filename)) Then
+                    Dim TempFileinfo As New FileInfo(Path.Combine(CStr(FolderDirectory(count)), Filename))
+                    Dim TempFileDate As Date = TempFileinfo.LastWriteTime
+                    Dim result As Integer = DateTime.Compare(FileDate, TempFileDate)
+                    'if result is less than 0 then the file is earlier than the backed up file, if result equals 0 then they are the same, 
+                    'if result is more than 0 then the file newer than the backuped file, so file needs to be copied over if newer
+                    'Only overwrite file over if checkbox is not ticked, this way only files that are not previously there will get copied across
+                    If result > 0 AndAlso cbNoOverwrite.Checked = False Then
+                        System.IO.File.Copy(FileLocation, Path.Combine(CStr(FolderDirectory(count)), Filename), True)
+                    End If
+                Else
+                    'file does not already exist so copy over
+                    System.IO.File.Copy(FileLocation, Path.Combine(CStr(FolderDirectory(count)), Filename))
+                End If
+            Next
+        Catch ex As Exception
+            MessageBox.Show("Something went wrong. " & ex.ToString, "Data error ")
+        End Try
+    End Sub
+    ''' <summary>
+    ''' This sub copies folder directories over to the backup location and uses recursion to call itself so that it looks at directories in itself and also calls CopyFiles to copy the files over in those directories
+    ''' </summary>
+    ''' <param name="Folder">This parameter holds the folder location</param>
+    ''' <param name="count">This parameter holds the count used to add more directories into the hashtable and used so that it creates a unique ID for each folder</param>
+    ''' <param name="FolderMarker">this parameter is used a marker so that it adds the correct folder directory path</param>
+    ''' <remarks></remarks>
+    Sub CopyDirectories(ByVal Folder As String, ByVal count As Integer, ByVal FolderMarker As Integer)
+        Try
+            'Check if folder exists in backup and if it doesnt copy entire folder over
+            'if folder exists then go through each folder and copy files/folders over
+            'use recursion to call 
+            Dim FolderInfo As New DirectoryInfo(Folder)
+            Dim Folderlocation As String = FolderInfo.FullName
+            Dim FolderName As String = FolderInfo.Name
+            Dim lstTempFolderDirectories As New List(Of String)
+            lstTempFolderDirectories.AddRange(Directory.GetDirectories(Folderlocation))
+
+            'if the folder does not exist then copy folder and contents over otherwise read through contents and update if necassary
+            If Directory.Exists(Path.Combine(CStr(FolderDirectory(count)), FolderName)) Then
+                lstAllFiles.Clear()
+                lstAllFiles.AddRange(Directory.GetFiles(Folderlocation))
+
+                'Loop until the count is higher than the countmarker so that the count is a new number and that the FolderDirectory doesnt try to add a directory in a used key
+                Do Until count > CountMarker
+                    count += 1
+                Loop
+                CountMarker = count
+
+                'Add directory to hashtable
+                FolderDirectory.Add((count), (Path.Combine(CStr(FolderDirectory(FolderMarker)), FolderName)))
+
+                'Add 1 to the foldermarker here
+                FolderMarker += 1
+
+                'Copy files in this directory over
+                CopyFiles(count)
+
+                If lstTempFolderDirectories.Contains(Nothing) Then
+                Else
+                    'Use recursion to call this same subroutine so that it can go through each subfolders
+                    For Each Folder In lstTempFolderDirectories
+                        CopyDirectories(Folder, count, FolderMarker)
+                    Next
+                End If
+            Else
+                'if folder does not exist then copy over whole of the directory including everything inside it
+                My.Computer.FileSystem.CopyDirectory(Folderlocation, Path.Combine(CStr(FolderDirectory(FolderMarker)), FolderName))
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show("Something went wrong. " & ex.ToString, "Data error ")
+        End Try
+    End Sub
+#End Region
+End Class
